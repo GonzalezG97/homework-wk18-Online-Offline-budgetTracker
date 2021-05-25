af8401e3e10ee8144079ea6e5a463a8133a113e1
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-05-25 13:22:41 -0500

    Added install event listener to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,4 +6,18 @@ const FILES_TO_Caches = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest"
-]
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
