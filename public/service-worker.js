a8ea72c673c151312e273987f757347b62c4d2b5
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-05-25 12:57:47 -0500

    Added files to cache in service worker

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_Caches = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest"
+]
