e64668d00a5b2a45c898a1f2f85b3b9e7a24abcd
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-05-28 12:43:52 -0500

    Set up checkDB ()

--- public/db.js
+++ public/db.js
@@ -14,14 +14,45 @@ request.onupgradeneeded = (e) => {
 };
 
 request.onsuccess = (e) => {
-    db = e.target.result;
-    if(navigator.onLine){
-        checkDB(){
-            
-        }
-    }
-}
+  db = e.target.result;
+  if (navigator.onLine) {
+    checkDB();
+  }
+};
 
 request.onerror = (e) => {
+  console.log("Error Fix it" + e.target.errorCode);
+};
 
-}
+const checkDB = () => {
+  console.log("DB invoked");
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  const store = transaction.objectStore("BudgetStore");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.results),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            currentStore.clear();
+            console.log("Clear store 🧹");
+          }
+        });
+    }
+  };
+};
